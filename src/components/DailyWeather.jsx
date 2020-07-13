eac2cb64974fc7547afeb5b83084669689f39d32
Ewelina <ewierzbicka3@gmail.com>
2020-07-13 18:57:12 +0200

    add long term weather forecast

--- src/components/DailyWeather.jsx
+++ src/components/DailyWeather.jsx
@@ -3,10 +3,29 @@ import React, { Component } from "react";
 export default class DailyWeather extends Component {
   render() {
     console.log(this.props);
+    let date = new Date(this.props.value.dt);
+    let day = date.getDate();
+    let month = date.getMonth();
+    let year = date.getFullYear();
+    console.log(date, month, year);
     return (
       <div>
-        {this.props.clouds && (
-          <div>Zachmurzenie: {this.props.clouds.all} </div>
+        {this.props.value && (
+          <div>
+            <p>
+              Data: {day}-{month}-{year}
+            </p>
+            <p>
+              Zachmurzenie:{" "}
+              {this.props.value.clouds.all || this.props.value.clouds}%{" "}
+            </p>
+            {this.props.value.main && (
+              <p>Temperatura: {this.props.value.main.temp}°C </p>
+            )}
+            {this.props.value.temp && (
+              <p>Temperatura: {this.props.value.temp.day}°C </p>
+            )}
+          </div>
         )}
       </div>
     );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,49 +1,92 @@
-import React from 'react';
-import DailyWeather from './DailyWeather'
+import React from "react";
+import DailyWeather from "./DailyWeather";
 
 class Form extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            value: '',
-            result: ''
-        };
-    
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    
-
-    handleChange(event) {
-        this.setState({ value: event.target.value })
-    }
-
-    getCity = () => {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${process.env.REACT_APP_OPEN_WEATHER_ID}&units=metric`)
-        .then(res => res.json())
-        .then(data => this.setState({ result: data }));                
+  constructor() {
+    super();
+    this.state = {
+      value: "",
+      city: "",
+      weather: [],
     };
-    
-    handleSubmit(event) {
-        event.preventDefault();
-        this.getCity();
-    }
-    
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                    Miasto:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                    </label>
-                    <input type="submit" value="Submit" />
-                </form>
-                <p>Wybrane miasto: {this.state.result.name}</p>
-                <DailyWeather clouds={this.state.result.clouds} />
-            </div>
-        );
-    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleDailySubmit = this.handleDailySubmit.bind(this);
+    this.handleLongTermSubmit = this.handleLongTermSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  getDailyWeather = () => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${process.env.REACT_APP_OPEN_WEATHER_ID}&units=metric`
+    )
+      .then((res) => res.json())
+      .then((data) =>
+        this.setState({
+          city: data.name,
+          weather: [data],
+        })
+      );
+  };
+
+  getLongTermWeather = () => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${process.env.REACT_APP_OPEN_WEATHER_ID}&units=metric`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&
+            exclude=current,minutely,hourly&appid=${process.env.REACT_APP_OPEN_WEATHER_ID}&units=metric`)
+          .then((res) => res.json())
+          .then((data) => this.setState({ weather: data.daily }));
+      });
+  };
+
+  handleDailySubmit(event) {
+    event.preventDefault();
+    this.getDailyWeather();
+  }
+
+  handleLongTermSubmit(event) {
+    event.preventDefault();
+    this.getLongTermWeather();
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <label>
+            Miasto:
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input
+            type="submit"
+            value="Wyświetl prognozę na dziś"
+            onClick={this.handleDailySubmit}
+          />
+          <input
+            type="submit"
+            value="Wyświetl prognozę długoterminową"
+            onClick={this.handleLongTermSubmit}
+          />
+        </form>
+        <p>Wybrane miasto: {this.state.city}</p>
+        <ul>
+          {this.state.weather.map((el) => (
+            <DailyWeather key={el.dt.toString()} value={el} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Form;
