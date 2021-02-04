040e93f499557ea5b3b596e2fb43cb37d4f5dec9
ewelina-wierzbicka <ewierzbicka3@gmail.com>
2021-02-04 17:22:37 +0100

    Updates

---
+++ precache-manifest.b2e5db6176c6a4d99d4d324dace108f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3be8b0949b005a3ab29ef725f6090e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49e5a31eb846ec1cb247",
+    "url": "/static/css/2.692ca9dc.chunk.css"
+  },
+  {
+    "revision": "220833c18ff4bfc00c60",
+    "url": "/static/css/main.e0af040b.chunk.css"
+  },
+  {
+    "revision": "49e5a31eb846ec1cb247",
+    "url": "/static/js/2.62a32d1e.chunk.js"
+  },
+  {
+    "revision": "885c0e205ef5e79f4aef448a818ba6a1",
+    "url": "/static/js/2.62a32d1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "220833c18ff4bfc00c60",
+    "url": "/static/js/main.d28e97b4.chunk.js"
+  },
+  {
+    "revision": "f294869ae1697dfe421d",
+    "url": "/static/js/runtime-main.7ecb0647.js"
+  },
+  {
+    "revision": "a614a0c54a2e647fec899579a5e3e353",
+    "url": "/static/media/Untitled-1.a614a0c5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather-app/precache-manifest.061adfda242803554c0b1068a71ab48a.js"
+  "/precache-manifest.b2e5db6176c6a4d99d4d324dace108f1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
